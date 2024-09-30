22fb3b99c0fc8b0fc87ffcc8664a9a79146c00a4
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-01 02:01:01 +0300

    password valudation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,12 @@ import { object, string} from 'yup';
 const Login = () => {
 
   const loginSchema = object({
-    password: string().required().min(8).max(16),
+    password: string().required("Şifre zorunludur").min(8, "Şifre en az 8 karakter içermelidir").max(16, "Şifre maksimum 16 karakter içerebilir").matches(/[a-z]+/, "Şifre en az bir küçük harf içermelidir")
+    .matches(/[A-Z]+/, "Şifre en az bir büyük harf içermelidir")
+    .matches(
+      /[@$!%*?&]+/,
+      "Şifre en az bir özel karakter (@$!%*?&) içermelidir"
+    ),
     email: string().email("Lütfen geçerli email giriniz").required(),
   })
 
